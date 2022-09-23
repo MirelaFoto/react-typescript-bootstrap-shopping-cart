84df4331de0b544339527990eff2fd32cdc03942
mirela <mirelafoto87@gmail.com>
2022-09-23 12:31:31 +0200

    react-typescript-cart

---
+++ src/pages/About.tsx
@@ -0,0 +1,4 @@
+export function About() {
+
+    return <h1>About, <hr></hr>This demo is for store/cart.<br></br>So please go to store!</h1>
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,6 @@
+
+
+export function Home() {
+
+    return <h1>Home, <hr></hr>This demo is for store/cart.<br></br>So please go to store!</h1>
+}
